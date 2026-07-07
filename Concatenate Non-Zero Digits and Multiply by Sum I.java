355cbffe85bc991c50c7135b265ad42742095e06
PRAVEEN KUMAR K <216186201+Praveenofficial12@users.noreply.github.com>
2026-07-07 17:14:23 +0530

    Create Concatenate Non-Zero Digits and Multiply by Sum I.java

---
+++ Concatenate Non-Zero Digits and Multiply by Sum I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long sumAndMultiply(int n) {
+        int x=0,s=0,p=1;
+        while(n>0){
+            int d=n%10;
+            if(d>0){
+                x+=d*p;
+                p*=10;
+                s+=d;
+            }
+            n/=10;
+        }
+        return 1L*x*s;
+    }
+}
