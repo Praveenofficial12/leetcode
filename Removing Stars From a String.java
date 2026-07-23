3799f2dd57a9e401f62ed704a0ea77c82b3b1fbe
PRAVEEN KUMAR K <216186201+Praveenofficial12@users.noreply.github.com>
2026-07-23 14:14:07 +0530

    Create Removing Stars From a String.java

---
+++ Removing Stars From a String.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String removeStars(String s) {
+        char [] arr=s.toCharArray();
+        int j=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]=='*') j--;
+            else {
+                arr[j]=arr[i]; 
+                j++;}
+        }
+        return new String(arr,0,j);
+    }
+}
